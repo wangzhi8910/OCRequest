e5472b3f677945c96d8ab38ec025bf8dd5ea80cd
zhi.wang <zhi.wang@2339.com>
2020-04-30 10:25:08 +0800

    1

---
+++ OCRequest/Classes/TestAB.h
@@ -0,0 +1,16 @@
+//
+//  TestAB.h
+//  AFNetworking
+//
+//  Created by 王志 on 2020/4/30.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestAB : NSObject
+@property (strong,nonatomic) NSString *aaa;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ OCRequest/Classes/TestAB.m
@@ -0,0 +1,12 @@
+//
+//  TestAB.m
+//  AFNetworking
+//
+//  Created by 王志 on 2020/4/30.
+//
+
+#import "TestAB.h"
+
+@implementation TestAB
+
+@end
